253428842bcbd1d61187fe2cf2f6111d5b7ad177
matchakay <zveupmjsgv@gmail.com>
2019-01-03 18:11:07 +0900

    2019/01/03-ok

--- app/src/main/java/com/matcha/kay/engineerreligion/MainActivity.java
+++ app/src/main/java/com/matcha/kay/engineerreligion/MainActivity.java
@@ -1,20 +1,12 @@
 package com.matcha.kay.engineerreligion;
 
-import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
